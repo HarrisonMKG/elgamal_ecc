80bca72e7e19a430ca091dbc31159b93c2377af9
Harrison Gordon <19hmkg@queensu.ca>
2023-11-20 18:51:21 -0500

    create script for calculating elgamal ecc

---
+++ elgamal_ecc.py
@@ -0,0 +1,46 @@
+gen = [3,10] # generator = [x,y]
+modulo = 23
+a_coeff = 1
+
+
+def point_addition(p_1,p_2):
+    lambda_coff = 0
+    x_3 = 0
+    y_3 = 0
+
+    if isinstance(p_1,str):
+        return p_2
+
+    if(p_1==p_2):
+        lambda_coff = ((3*pow(p_1[0],2)+a_coeff)%modulo)*(pow(2*p_1[1],-1,modulo))
+        lambda_coff =  lambda_coff % modulo
+
+    else:
+        delta_x = (p_2[0] - p_1[0]) % modulo 
+        delta_y = (p_2[1] - p_1[1]) % modulo
+        if 0==delta_x:
+            return "inf"
+        lambda_coff = (delta_y)*(pow(delta_x,-1,modulo))
+        lambda_coff =  lambda_coff % modulo
+
+    x_3 = pow(lambda_coff,2) - p_1[0] - p_2[0]
+    x_3 = x_3 % modulo 
+    y_3 = (p_1[0] - x_3)*lambda_coff - p_1[1]
+    y_3 = y_3 % modulo
+    return ([x_3,y_3])
+
+
+def main():
+    point = point_addition(gen,gen) 
+    index = 1
+    group = {}
+    group[0] = gen
+    while index != 33:
+        group[index]=point
+        index = index + 1
+        point = point_addition(point,gen) 
+        print(f"{point}={group[index-1]}+{gen}[x,y]")
+    print(f"group: {group}")
+
+
+main()
